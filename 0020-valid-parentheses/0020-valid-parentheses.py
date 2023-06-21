6e4e9fb8ef330cb0201b8fb8e8713699ac32385f
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-20 23:26:21 -0400

    Time: 24 ms (23.59%), Space: 13.5 MB (63.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -21,15 +21,17 @@ class Solution(object):
         for index, char in enumerate(s):
           # if char is closing bracket, check stack
           if char in closing_brackets:
+            # if match, pop bracket
             if stack and closing_brackets[char] == stack[-1]:
               stack.pop()
+            # else, immediately return false
             else:
               return False
           # else, add char to stack
           else:
             stack.append(char)
         
-        # if stack is empty
+        # EDGE CASE: if stack is empty
         if not stack:
           return True
         # if stack still has leftover bracket(s)
