17eb795c279c02adfabb36f41575ea855d976486
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-25 23:31:11 -0400

    Time: 67 ms (34.07%), Space: 14 MB (83.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -12,17 +12,17 @@ class Solution(object):
         empty string
         """
 
-        processedString = ""
+        # processedString = ""
 
-        for char in s:
-          # to check if it's alphanumeric, 
-          # i.e. ignore whitespaces and special characters
-          if char.isalnum():
-            # .lower() to ignore cases
-            processedString += char.lower()
+        # for char in s:
+        #   # to check if it's alphanumeric, 
+        #   # i.e. ignore whitespaces and special characters
+        #   if char.isalnum():
+        #     # .lower() to ignore cases
+        #     processedString += char.lower()
 
-        # [::-1] reverses a string, so just check for equality
-        return processedString == processedString[::-1]
+        # # [::-1] reverses a string, so just check for equality
+        # return processedString == processedString[::-1]
 
 
         """
@@ -39,9 +39,9 @@ class Solution(object):
         while l < r:
           # increment / decrement l and r while
           # they are NOT alphanumeric
-          while l < r and not alphaNum(s[l]):
+          while l < r and not self.alphaNum(s[l]):
             l += 1
-          while r > l and not alphaNum(s[r]):
+          while r > l and not self.alphaNum(s[r]):
             r -= 1
 
           # ok, both are alphanumeric, now
@@ -49,14 +49,19 @@ class Solution(object):
           if s[l].lower() != s[r].lower():
             return False
 
+          # increment / decrement l and r after check(s)
+          l, r = l + 1, r - 1
+        
+        return True
 
-        # ord() gets the ascii value of characters
-        # this is our own custom alphaNum func, 
-        # highly customizable
-        def alphaNum(self, c):
-          return (ord('A') <= ord(c) <= ord('Z') or
-                  ord('a') <= ord(c) <= ord('z') or
-                  ord('0') <= ord(c) <= ord('9'))
+
+    # ord() gets the ascii value of characters
+    # this is our own custom alphaNum func, 
+    # highly customizable
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
 
 
         
