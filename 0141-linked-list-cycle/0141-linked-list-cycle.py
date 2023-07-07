7f9085b20ac9e043f7918aa9fbd30848d1a625f2
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-07-06 23:12:32 -0400

    Time: 50 ms (29.91%), Space: 20.4 MB (64.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -16,14 +16,36 @@ class Solution(object):
         not value because there might be duplicates
         """
 
-        hashMap = set()
+        # hashMap = set()
 
-        while (head):
-          if head in hashMap:
+        # while (head):
+        #   if head in hashMap:
+        #     return True
+        #   else:
+        #     hashMap.add(head)
+        #   head = head.next
+        # return False
+
+        """
+        more optimized version,
+        doesn't require O(n) space
+        https://www.youtube.com/watch?v=gBTe7lFR3vc
+        """
+
+        slow, fast = head, head
+
+        # we need to check both fast and fast.next
+        # that they're not null since we're moving
+        # the fast pointer by 2
+        while fast and fast.next:
+          slow = slow.next
+          fast = fast.next.next
+          if slow == fast:
             return True
-          else:
-            hashMap.add(head)
-          head = head.next
         return False
+          
+
+
+
           
           
