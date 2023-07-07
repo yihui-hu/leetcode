69c50d698ae9946b08b112dc87aae3e7acf726ea
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-07-06 23:13:41 -0400

    Time: 39 ms (83.17%), Space: 20.3 MB (64.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -37,6 +37,11 @@ class Solution(object):
         # we need to check both fast and fast.next
         # that they're not null since we're moving
         # the fast pointer by 2
+        # say you have a gap of 10 between the slow and fast pointer.
+        # each iteration, the gap decreases -> 10 + 1 - 2 = 9
+        # so if there's a loop, eventually the gap will be 0
+        # and the slow and fast pointer will meet
+        # otherwise, fast / fast.next reaches null and so there's no loop
         while fast and fast.next:
           slow = slow.next
           fast = fast.next.next
