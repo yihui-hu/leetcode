919ad213d89b502630a0b1473468acb39675a3fe
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-08-23 15:32:48 -0400

    Time: 65 ms (44.03%), Space: 16.3 MB (60.79%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,60 +1,35 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        time, fresh = 0, 0
 
-      """
-      perform multi-source BFS on the rotten oranges
-      and we also keep track of fresh oranges
+        # initialize queue and keep track of fresh
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    q.append([r, c])
+                elif grid[r][c] == 1:
+                    fresh += 1
+        
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-      when the queue we use for BFS is empty, we know all rotten
-      oranges have finished 'infecting' other oranges; if there
-      are still fresh oranges, we return -1
+        # now, run through queue and perform multi source BFS
+        while q and fresh > 0:
+            # to perform multi source BFS, pop all items off and run BFS same time
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for r2, c2 in directions:
+                    new_r, new_c = r + r2, c + c2
 
-      each time we pop from the queue, we add 1 unit of time
-      """
+                    if 0 <= new_r < ROWS and 0 <= new_c < COLS and grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
+                        q.append([new_r, new_c])
+                        fresh -= 1
 
-      q = deque()
-      time, fresh = 0, 0
-
-      ROWS, COLS = len(grid), len(grid[0])
-      # iterate over entire grid to find all oranges
-      for r in range(ROWS):
-        for c in range(COLS):
-          if grid[r][c] == 1:
-            fresh += 1
-          elif grid[r][c] == 2:
-            q.append([r, c])
-      
-      # perform multi source BFS
-      while q and fresh > 0:
-        # pop all elems off queue and perform BFS on all of them
-        for _ in range(len(q)):
-          r, c = q.popleft()
-          # get top
-          if r > 0:
-            if grid[r - 1][c] == 1:
-              grid[r - 1][c] = 2
-              q.append([r - 1, c])
-              fresh -= 1
-          # get bottom
-          if r < ROWS - 1:
-            if grid[r + 1][c] == 1:
-              grid[r + 1][c] = 2
-              q.append([r + 1, c])
-              fresh -= 1
-          # get left
-          if c > 0:
-            if grid[r][c - 1] == 1:
-              grid[r][c - 1] = 2
-              q.append([r, c - 1])
-              fresh -= 1
-          # get right
-          if c < COLS - 1:
-            if grid[r][c + 1] == 1: 
-              grid[r][c + 1] = 2
-              q.append([r, c + 1])
-              fresh -= 1
-        time += 1
-
-      print(fresh)
-
-      return time if fresh == 0 else -1
+            time += 1
+        
+        return time if fresh == 0 else -1
