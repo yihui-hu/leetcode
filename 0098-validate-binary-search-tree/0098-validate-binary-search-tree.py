891a36b716e0514c2c473313d7746db0036ad5d6
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-08-24 10:49:53 -0400

    Time: 39 ms (97.82%), Space: 18.9 MB (31.65%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+      """
+      intuition: perform DFS
+      check from bottom up if they're valid BSTs
+      """
+
+      """
+      THIS SOLUTION DOES NOT WORK
+
+      [5, 4, 6, null, null, 3, 7]
+
+             5
+            / \
+           4   6
+              / \
+             3   7
+
+      Note that they're all valid subtrees but since 3 is to the right
+      of 5, this should return False, but the soln returns True.
+
+      Need to do better...
+
+      def dfs(root):
+        # base case: node has no children
+        if not root:
+          return True
+
+        # recurse on left and right children
+        left = dfs(root.left)
+        right = dfs(root.right)
+        
+        if left and right:
+          leftValid, rightValid = False, False
+          if not root.left or (root.left and root.left.val < root.val):
+            leftValid = True
+          if not root.right or (root.right and root.right.val > root.val):
+            rightValid = True
+          return leftValid and rightValid
+        else:
+          return False
+
+      return dfs(root)
+      """
+
+      # define boundaries via left and right
+      def valid(node, left, right):
+        if not node:
+          return True
+        
+        if not (node.val < right and node.val > left):
+          return False
+        
+        # have to check the following cond: left < node.left < node.val
+        return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+      
+      # -infinity < root < infinity, since the root can be any value
+      return valid(root, float("-inf"), float("inf"))
