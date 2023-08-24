989e181869ac605ba52b1c133e80f17faf83d46b
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-08-24 14:49:34 -0400

    Time: 47 ms (58.34%), Space: 16.5 MB (67.58%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+      
+      """
+      perform backtracking
+      """
+
+      result = []
+
+      # base case
+      if (len(nums) == 1):
+        return [nums[:]]
+      
+      for i in range(len(nums)):
+        # pop first value off and get permutations of the other vals
+        n = nums.pop(0)
+        perms = self.permute(nums)
+
+        for perm in perms:
+          perm.append(n)
+        result.extend(perms)
+        nums.append(n)
+
+      return result
+
+
+
