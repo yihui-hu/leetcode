f519fd0321d4c334fa02147a18890766fbb4917d
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-08-28 16:11:25 -0400

    Time: 535 ms (75.91%), Space: 31 MB (83.6%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -7,6 +7,29 @@ class Solution:
 
       slow = fast = 0
 
+      """
+      because the elements are within the range of [1, n],
+      we can treat the array as a representation of a linked list
+
+      e.g.
+      [0, 1, 2, 3, 4]
+      [1, 3, 4, 2, 2]
+
+      0 -> 3 -> 2 -> 4
+                  <-
+      
+      cycle with 2 and 4
+
+      first phase:
+      advance slow and fast pointers until they intersect
+
+      second phase:
+      create second slow pointer
+      advance both slow pointers until they intersect
+      see proof here: https://www.youtube.com/watch?v=wjYnzkAhcNk
+      but tldw; where they intersect is the index / val of the repeated num
+      """
+
       while True:
         slow = nums[slow]
         fast = nums[nums[fast]]
