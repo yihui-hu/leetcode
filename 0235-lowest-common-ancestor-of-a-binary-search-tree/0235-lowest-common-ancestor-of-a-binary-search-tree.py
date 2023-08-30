1a6071ab2971cc2da82f8956ac64303543e0a500
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-08-30 12:16:45 -0400

    Time: 75 ms (69.17%), Space: 20.9 MB (22.76%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,41 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+      """
+      if the two values are both greater than or less than current node,
+      we can move to the right or left subtree respectively
 
-        """
-        edge cases:
-        1. one of the nodes is root or ancestor of the other
-        intuition:
-        lowest common ancestor is the *first* split of p and q
-        i.e. since this is a binary tree, once either p or q
-        ends up on either side of a node, we know that node is the LCA
-        either that or the node is either p or q itself, then we know
-        that node will be LCA because the other will be lower down the tree
-        """
+      if they're on different sides, then we know that the current node
+      is the LCA
 
-        curr = root
+      if one of them is the curr node, then that node is the LCA
+      """
 
-        # while curr not null i.e. keep traversing tree
-        # lmao remember to do .val to get values
-        while curr:
-          # no split yet, both p and q are larger than curr node
-          if p.val > curr.val and q.val > curr.val:
-            curr = curr.right
-          # no split yet, both p and q are smaller than curr node
-          elif p.val < curr.val and q.val < curr.val:
-            curr = curr.left
-          # means that we've either split or we've found either p or q
-          else:
-            return curr
+      curr = root
+
+      while curr:
+        if p.val > curr.val and q.val > curr.val:
+          curr = curr.right
+        elif p.val < curr.val and q.val < curr.val:
+          curr = curr.left
+        else:
+          return curr
