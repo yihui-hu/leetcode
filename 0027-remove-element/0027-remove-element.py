4f4f515d18966b9938a08688761ce617ee17ece1
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-08-30 16:07:28 -0400

    Time: 40 ms (74.75%), Space: 16.3 MB (69.18%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -3,8 +3,15 @@ class Solution:
       index = 0
 
       for num in nums:
+        # since the rest of the array doesn't matter,
+        # we can just overwrite it like so
         if num != val:
           nums[index] = num
           index += 1
+
+      """
+      [3, 2, 2, 3] val = 3
+      [2, 2, 2, 3] and index ends up at 2
+      """
       
       return index
