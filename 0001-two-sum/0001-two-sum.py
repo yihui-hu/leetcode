678b135c2d1ea73806debce4a69d0bba05dc380d
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-10 10:15:44 -0400

    Time: 66 ms (70.48%), Space: 17.7 MB (13.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,33 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
 
-        """
-        1. use hash map
-        2. store as value : index
-        """
-
-        """
-        edge cases:
-        1. negative numbers
-        2. duplicate numbers (should be ok as long as we iterate)
-        """
-
-        prevMap = {} # mapping value : index
-
-        # loop through array of nums
-        for index, value in enumerate(nums):
-            # get difference between target and current num
-            difference = target - value
-            # if difference exists in hash map, great, return both indices
-            if difference in prevMap:
-                return [prevMap[difference], index]
-            # else, add current value and its index to hash map
-            prevMap[value] = index
-
-        return []
-        
+        for i in range(len(nums)):
+          complement = target - nums[i]
+          if complement in map:
+            return [i, map[complement]]
+          else:
+            map[nums[i]] = i
+            
