c35351bcae7e4d7817d9645d147f6ddd72eba464
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-11 09:59:03 -0400

    Time: 810 ms (23.67%), Space: 23.2 MB (33.11%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,47 +1,25 @@
-class Solution(object):
-    def kClosest(self, points, k):
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
+        high level understanding: we want to use a minHeap
+        what do we store in a minHeap? a tuple (distance, coords)
+        we sort the minheap on distance, and when we retrieve them,
+        we concat the coords in the tuple into an array to return it
         """
-
-        """
-        intuition:
-        iterate through points,
-        calculate distance to origin
-        add to a min-heap / priority queue
-        then extract k times from min-heap
-        """
-
-        """
-        for later: refresh how heapify works in theory
-        """
-
-        import heapq
 
         minHeap = []
-
-        # iterate through all points
-        for point in points:
-          x = point[0]
-          y = point[1]
-          # don't need to calculate square root
-          dist = (x ** 2) + (y ** 2)
-          # we put distance as the first elem because
-          # that's what we want to order the elems by
-          minHeap.append([dist, x, y])
-
-        # heapify minHeap array
-        heapq.heapify(minHeap)
-        result = []
-
-        # now add back to result array by popping
-        # elements off the heap
-        for i in range(k):
-          dist, x, y = heapq.heappop(minHeap)
-          result.append((x, y))
         
-        return result
+        for coords in points:
+          dist = sqrt((coords[0] - 0) ** 2 + (coords[1] - 0) ** 2)
+          minHeap.append((dist, coords))
+        
+        heapq.heapify(minHeap)
 
+        print(minHeap)
 
+        res = []
+        for i in range(k):
+          tup = heapq.heappop(minHeap)
+          res.append(tup[1])
+
+        return res
