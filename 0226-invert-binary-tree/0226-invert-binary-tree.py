db367aca369823937672a62fada7458602f1bd1a
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-10 23:30:48 -0400

    Time: 34 ms (88.42%), Space: 16.3 MB (30.11%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,37 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-
-        """
-        use recursion to traverse tree and swap children
-        use DFS to visit every single node, and swap children
-        edge cases:
-        1. empty tree
-        2. tree with only one node (i.e. only root)
-        3. check if children exists
-        """
-
-        # if root is null, tree doesn't exist, simply return None
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
           return None
         
-        # swap children
-        tmp = root.left
+        temp = root.left
         root.left = root.right
-        root.right = tmp
+        root.right = temp
 
-        # recurse on the left and right children
         self.invertTree(root.left)
         self.invertTree(root.right)
 
-        # remember to return the (new) root!
         return root
+
