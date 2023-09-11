b59745111d57ad8f0009a751a499d0b31919c0f1
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-11 19:54:08 -0400

    Time: 54 ms (64.44%), Space: 21.2 MB (33.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,43 +1,32 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isBalanced(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+      """
+      height balanced means that subtrees must have height difference 
+      no greater than 1
+      use DFS
+      """
 
-        """
-        because of the definition of height-balanced
-        we want to check from the bottom up
-        if we do top down it will be O(n * n)
-        since we have to check height of subtrees for each node
-        """
+      def dfs(node):
 
-        # we need this helper function to return
-        # a boolean (is it balanced or not?) and the height of the (sub)tree
-        def dfs(root):
-            if not root: return [True, 0]
+        # recurse all the way down
+        # check its children first (get height of children)
+        # compare height of children, if greater than 1 return False
+        # current node, assign the max height of its two children
 
-            left, right = dfs(root.left), dfs(root.right)
-
-            # remember dfs() return [isBalanced, height]
-            # so left[0], right[0] tells us if (sub)tree is balanced
-            # and left[1], right[1] tells us the height(s) of the (sub)trees
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-
-            # return to parent
-            return [balanced, 1 + max(left[1], right[1])]
-    
-        # perform dfs on the root, return first item i.e. isBalanced
-        return dfs(root)[0]
-
-
-
-
-
+        if not node:
+          return [True, 0]
         
+        left = dfs(node.left)
+        right = dfs(node.right)
+
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+
+        return [balanced, max(left[1], right[1]) + 1]
+
+      return dfs(root)[0]
