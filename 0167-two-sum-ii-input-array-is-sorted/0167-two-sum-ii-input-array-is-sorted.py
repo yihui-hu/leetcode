6618733eeed48aa29389dc45746456b64476ad87
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-15 11:00:08 -0400

    Time: 121 ms (57.52%), Space: 17.1 MB (91.24%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,32 +1,17 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        """
-        intuition: use binary search
-        search from the middle:
-          if smaller than target, vals are all on left of mid
-          if larger than or equal to target, vals are all on right of mid
-
-        another approach:
-        use two pointers (one on each end)
-        depending on sum of two pointers, move either left or right
-        finish when l >= r (either found or not found)
+        use two pointers, one at start and end
+        move them according to if theyre bigger / smaller than targer
         """
 
-        l, r = 0, len(numbers) - 1
+        left = 0
+        right = len(numbers) - 1
 
-        while l < r:
-          currSum = numbers[l] + numbers[r];
-          if currSum < target:
-            l += 1
-          elif currSum > target:
-            r -= 1
+        while numbers[left] + numbers[right] != target:
+          if numbers[left] + numbers[right] > target:
+            right -= 1
           else:
-            return [l + 1, r + 1]
-        
-        return
+            left += 1
+
+        return [left + 1, right + 1]
