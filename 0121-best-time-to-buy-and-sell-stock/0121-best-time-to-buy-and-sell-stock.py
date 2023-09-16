509df4be991dcf6af1d1bc6e88ad4a57629477aa
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-16 10:34:47 -0400

    Time: 869 ms (58.47%), Space: 27.4 MB (56.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,13 @@ class Solution:
         
         max_profit = 0
         while right < len(prices):
+          # calculate curr profit, check if it's > curr max profit
           if prices[left] < prices[right]:
             max_profit = max(max_profit, prices[right] - prices[left])
           else:
+            # we found a new minimum at r, so we move l to r
             left = right
+          # always increment r
           right += 1
         
         return max_profit
