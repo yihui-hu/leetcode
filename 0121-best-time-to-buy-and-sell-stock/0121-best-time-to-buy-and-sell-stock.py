8b275f4df22fd8ae94429a9638f04174e963a951
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-16 10:33:55 -0400

    Time: 875 ms (53.18%), Space: 27.3 MB (56.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,30 +1,13 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-
-        """
-        naive solution: try all combinations, n^2
-        """
-
-        # left = buy index, right = sell index
-        l, r = 0, 1
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        
         max_profit = 0
-
-        # iterate through entire prices array
-        while r < len(prices):
-          # if we have profit, calculate current profit and
-          # check if it's lower than our current max profit
-          if prices[l] < prices[r]:
-            current_profit = prices[r] - prices[l]
-            max_profit = max(max_profit, current_profit)
+        while right < len(prices):
+          if prices[left] < prices[right]:
+            max_profit = max(max_profit, prices[right] - prices[left])
           else:
-            # instead of moving by just one spot, move l to r because
-            # we found a new lower price
-            l = r
-          # move right pointer regardless of condition
-          r += 1
+            left = right
+          right += 1
         
         return max_profit
