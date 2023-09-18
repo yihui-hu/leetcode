c1e4dd47bd2ef6da73da38c383f96893a0f9c46f
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-17 20:29:37 -0400

    Time: 67 ms (93.89%), Space: 16.4 MB (89.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,44 +1,27 @@
-class Solution(object):
-    def floodFill(self, image, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-        """
-        use DFS to recursively fill image
-        edge cases:
-        1. pixel is already new color
-        2. pixel is only color of its kind
-        assumptions:
-        1. uniform image (set number of rows and columns, rectangular is ok)
-        """
+      """
+      perform bfs
+      """
 
-        # if image is already the new color, return
-        if image[sr][sc] == color:
-          return image
-        
-        # call fill function
-        # image[sr][sc] represents current / prevColor in recursive calls
-        self.fill(image, sr, sc, color, image[sr][sc])
+      rows, cols = len(image), len(image[0])
+      og_color = image[sr][sc]
 
-        # return filled image
+      if image[sr][sc] == color:
         return image
 
-    # recursive function for filling
-    # base case: out of bounds pixel or not same color
-    def fill(self, image, sr, sc, color, prevColor):
-      if sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[0]) or image[sr][sc] != prevColor:
-        return
-      else:
-        # change pixel to new color
-        image[sr][sc] = color
-
-        # recursive call on neighbouring pixels
-        self.fill(image, sr-1, sc, color, prevColor)
-        self.fill(image, sr, sc-1, color, prevColor)
-        self.fill(image, sr+1, sc, color, prevColor)
-        self.fill(image, sr, sc+1, color, prevColor)
+      def bfs(r, c):
+        if r not in range(rows) or c not in range(cols) or image[r][c] != og_color:
+          return
+        else:
+          image[r][c] = color
+          bfs(r + 1, c)
+          bfs(r - 1, c)
+          bfs(r, c + 1)
+          bfs(r, c - 1)
+      
+      bfs(sr, sc)
+      return image
+        
+        
