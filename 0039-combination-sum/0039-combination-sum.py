04009a2e6e566e31f745478f8054db01e9105b6f
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-09-18 09:55:11 -0400

    Time: 79 ms (20.04%), Space: 16.4 MB (51.95%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -11,28 +11,46 @@ class Solution:
 
       res = []
 
-      # i = maintains which of the candidate we include
-      # curr = tells us what we've added to the current comb so far
-      # total = total sum of elems in curr
-      def dfs(i, curr, total):
-        # base case
-        if total == target:
-          res.append(curr.copy()) # add combination to the res array
+      def dfs(index, candidate):
+        if sum(candidate) == target:
+          res.append(candidate.copy())
           return
-
-        if i >= len(candidates) or total > target:
+        
+        if index >= len(candidates) or sum(candidate) > target:
           return
+        
+        # now we make choices
 
-        # here, we run the two branch decision DFS 
+        candidate.append(candidates[index])
+        dfs(index, candidate)
 
-        # first decision where we include the candidate
-        curr.append(candidates[i])
-        dfs(i, curr, total + candidates[i])
-
-        curr.pop() # remove the elem from the last decision we made
-
-        # second decision where we exclude the candidate
-        dfs(i + 1, curr, total) 
-
-      dfs(0, [], 0)
+        candidate.pop()
+        dfs(index + 1, candidate)
+      
+      dfs(0, [])
       return res
+
+      res = []
+
+      # # i = maintains which of the candidate we include
+      # # curr = tells us what we've added to the current comb so far
+      # # total = total sum of elems in curr
+      # def dfs(i, curr, total):
+      #   # base case
+      #   if total == target:
+      #     res.append(curr.copy()) # add combination to the res array
+      #     return
+
+      #   if i >= len(candidates) or total > target:
+      #     return
+
+      #   # first decision where we include the candidate
+      #   curr.append(candidates[i])
+      #   dfs(i, curr, total + candidates[i])
+
+      #   # second decision where we exclude the candidate
+      #   curr.pop()
+      #   dfs(i + 1, curr, total) 
+
+      # dfs(0, [], 0)
+      # return res
